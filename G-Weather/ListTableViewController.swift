6f70da1cf67040496dfc7789a085dfee9a8a587a
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-02-18 19:25:08 +0300

    сделал запрос к яндексу

---
+++ G-Weather/ListTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ListTableViewController.swift
+//  G-Weather
+//
+//  Created by Сперанский Никита on 18.02.2022.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fetchWeather()
+        
+    }
+    
+    func fetchWeather() {
+        
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.93565577193928&lon=30.32214469739418"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.addValue("9cf882c0-5b86-411c-9560-ca353cf27fc5", forHTTPHeaderField: "X-Yandex-API-Key")
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, responce, error) in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
